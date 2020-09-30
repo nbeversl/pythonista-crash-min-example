8b35512c51d36358ca449065329bbf6ef551f883
n_beversluis <nbeversluis@wttw.com>
2020-09-30 12:11:34 -0500

    comments out problem line

--- main.py
+++ main.py
@@ -80,7 +80,8 @@ class MainView(ui.View):
 		self.tv.text_color = 'white'
 		self.tv.width = w
 		
-		self.full_txt_search_field = ui.TextField()
+		# un-comment this line to cause a crash:
+		#self.full_txt_search_field = ui.TextField()
 
 		self.add_subview(self.full_txt_search_field)		
 		self.add_subview(self.tv)
